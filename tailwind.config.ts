28590629f22baf9553e45527590b353f3fa5e75d
kangsinbeom <kanian16@naver.com>
2024-03-11 11:03:18 +0900

    design: bg-gray 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,6 +20,7 @@ const config = {
     },
     extend: {
       colors: {
+        mybg: "#f0f0f0",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
